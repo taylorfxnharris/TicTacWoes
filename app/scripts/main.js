a377e86151879d86ee7bd20ba8822f06455b364d
Taylor Harris <thharris10@gmail.com>
2015-08-25 12:25:33 -0400

    got it to reset but wont let player play again

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,83 +1,123 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
 
 //array
 var playerTurn = 1;     //we set a variable to remember the va
 var squares = [0,0,0,
 			   0,0,0,
 			   0,0,0]   //Setting the 0 to the players ID,
-
+var player; //telling the script we are creating a variable.
+var winner;
 // we made a player that can click, whos turn
+
 
 $('.square').bind('click', function(event){  //this allows you to click once and not change the character
 {$(this).unbind('click');}
 	console.log($(this).attr('id'))   	//getting the iD that I set in the DIV, turning it into a number instead of a word
 		var id = ($(this).attr('id'))   // saving what I printed into an ID 
-		id = parseInt(id)				//parse int takes  a string and turns it into an integer
+		id = parseInt(id)				//parse int takes  a string and turns it into an integer, I have to make sure its an integer
 		squares[id] = playerTurn
-		playerTurn == 1 || playerTurn == 2 //
+		playerTurn == '1' || playerTurn == '2' //
 		console.log(squares)
-
 		
 //modulo
 // horizontal
-
+// function
 //Winner
-	if (squares[0] == squares[1] && squares[1] == squares[2] && squares[0] == squares[2] && squares[0] != 0){  //have to do more because they all equal zero
-		$('.winner').text('winner' + playerTurn);
-		('player wins')
-		console.log(playerTurn)
-	}
-	if (squares[3] == squares[4] && squares[4] == squares[5] && squares[3] == squares[5] && squares[3] != 0){  //have to do more because they all equal zero
-		('player wins')
-		console.log(playerTurn)
-	}
-	if (squares[6] == squares[7] && squares[7] == squares[8] && squares[6] == squares[8] && squares[6] != 0){  //have to do more because they all equal zero
-		('player wins')
-		console.log(playerTurn)
-	}
-// vertical 
-	if (squares[0] == squares[3] && squares[3] == squares[6] && squares[0] == squares[6] && squares[0] != 0){  //have to do more because they all equal zero
-		('player wins')
-		console.log(playerTurn)
-	}
-	if (squares[1] == squares[4] && squares[4] == squares[7] && squares[1] == squares[7] && squares[1] != 0){  //have to do more because they all equal zero
-		('player wins')
-		console.log(playerTurn)
-	}
-	if (squares[2] == squares[5] && squares[5] == squares[8] && squares[2] == squares[8] && squares[2] != 0){  //have to do more because they all equal zero
-		('player wins')
-		console.log(playerTurn)
-	}
-// diagonal
-	if (squares[0] == squares[4] && squares[4] == squares[8] && squares[0] == squares[8] && squares[0] != 0){  //have to do more because they all equal zero
-		('player wins')
-		console.log(playerTurn)
-	}
-	if (squares[2] == squares[4] && squares[4] == squares[6] && squares[2] == squares[6] && squares[2] != 0){  //have to do more because they all equal zero
-		('player wins')
-		console.log(playerTurn)
-	}
+//clear out HTML
 
-	
-if (playerTurn == 1) { 
+
+
+	if (playerTurn == 1) { 
 		$(this).css({'background-image': 'url(/images/drakehead.jpg)',
 					 'background-attachment': 'cover',
      				 'background-size': '100%'}); 
 		playerTurn = 2  	}
 
-		else if (playerTurn == 2) {
+	else if (playerTurn == 2) {
 		$(this).css({'background-image': 'url(/images/meekhead.jpg)',
 					 'background-attachment': 'cover',
      				 'background-size': '100%'});
 		playerTurn = 1
 	}
+	// else if (player == )
 
 
-//reset
+
+	if (squares[0] == squares[1] && squares[1] == squares[2] && squares[0] != 0){  //have to do more because they all equal zero
+		player = (playerTurn % 2 == 0 ? "Drake" : "Meek")     //it knows about the variable and equals the result
+		$('.winner').text('winner is: ' + player);
+		window.setTimeout(reset, 5000);
+		
+
+		// add breaks break the cycle break the code, exit, and reset the board, grabbing all divs, set HTML to an empty string.remvoe whatever is within the css remove it
+		console.log(player)  //not on variables
+	}
+	
+	else if (squares[3] == squares[4] && squares[4] == squares[5] && squares[3] == squares[5] && squares[3] != 0){  //have to do more because they all equal zero
+		('player wins')
+		console.log(playerTurn)
+	}
+	else if (squares[6] == squares[7] && squares[7] == squares[8] && squares[6] == squares[8] && squares[6] != 0){  //have to do more because they all equal zero
+		('player wins')
+		console.log(playerTurn)
+	}
+// vertical 
+	else if (squares[0] == squares[3] && squares[3] == squares[6] && squares[0] == squares[6] && squares[0] != 0){  //have to do more because they all equal zero
+		('player wins')
+		console.log(playerTurn)
+	}
+	else if (squares[1] == squares[4] && squares[4] == squares[7] && squares[1] == squares[7] && squares[1] != 0){  //have to do more because they all equal zero
+		('player wins')
+		console.log(playerTurn)
+	}
+	else if (squares[2] == squares[5] && squares[5] == squares[8] && squares[2] == squares[8] && squares[2] != 0){  //have to do more because they all equal zero
+		('else player wins')
+		console.log(playerTurn)
+	}
+// diagonal
+	else if (squares[0] == squares[4] && squares[4] == squares[8] && squares[0] == squares[8] && squares[0] != 0){  //have to do more because they all equal zero
+		('player wins')
+		console.log(playerTurn)
+	}
+	else if (squares[2] == squares[4] && squares[4] == squares[6] && squares[2] == squares[6] && squares[2] != 0){  //have to do more because they all equal zero
+		('player wins')
+												.log(playerTurn)
+	
+}
+	//else 
+	//its a tie
+	
 
 
-})
+
+
+
+
+});
+
+$('.reset').click(function() { //reset to clear before game ends
+	reset();
+});
+
+var reset = function() {
+	squares = 	  [0,0,0,
+			  	   0,0,0,
+			   	   0,0,0];		   	   
+	console.log(squares);
+	$('.square').removeAttr('style');   //removing the styling but now add the bind
+	//logic to reset board
+
+}
+	
+
+	// jquery clear all images and rebind squares
+	// reset square array
+	//reset
+	// add a function that would clear 
+
+
+
+
 //ideas
 // order is important winner
 //background url instead of red
@@ -87,7 +127,6 @@ if (playerTurn == 1) {
 //every time I move it does something
 //then delay the move... 
 //use jquery to say who wins
-
 //Selecting every div named square, you click it says something
 
 //click div change player turns // single equal clicks it to the variable
